cdd104c2e8715f04a2789bd2c5175d71eb444798
JUAN FRANCISCO MARTINEZ <juan.martinez@bsc.es>
2016-10-14 16:58:34 +0200

    Some simple tests

---
+++ tests/loop_nest.c
@@ -0,0 +1,79 @@
+/*
+ * loop.c
+ * Copyright (C) 2016 Juan Francisco Martínez <juan.martinez[AT]bsc[dot]es>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "mpi.h"
+
+#define ITERATIONS 20
+#define ITERATIONS_NESTED 10
+#define MASTER 0
+#define TAG 1234
+#define TOSLEEP 500
+#define RAND_SEED 549384
+
+int main(int argc, char **argv)
+{
+	int rank, team;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &team);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	if (team%2 != 0)
+	{
+		printf("Team number must be pair.\n");
+		MPI_Finalize();
+		exit(1);
+	}
+
+	if (rank == MASTER)
+		printf("Team: %d\n", team);
+
+	srand(RAND_SEED);
+
+	int i=0;
+	for (i; i<ITERATIONS; ++i)
+	{
+		if (rank%2==0)
+		{
+			int prank;
+			MPI_Send(&rank, 1, MPI_INT, rank+1, TAG, MPI_COMM_WORLD);
+			MPI_Recv(&prank, 1,MPI_INT, rank+1, TAG, MPI_COMM_WORLD, NULL);
+		}	
+		else
+		{
+			int prank;
+			MPI_Recv(&prank, 1,MPI_INT, rank-1, TAG, MPI_COMM_WORLD, NULL);
+			MPI_Send(&rank, 1, MPI_INT, rank-1, TAG, MPI_COMM_WORLD);
+		}
+
+		// Perform some work
+		usleep(TOSLEEP);
+
+		int j=0;
+		for (j; j<ITERATIONS_NESTED; ++j)
+		{
+			int val;
+			if (rank==MASTER)
+				val=rand();
+			MPI_Bcast(&val, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
+		}
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	if (rank == MASTER)
+		printf("End of execution reached.");
+
+	MPI_Finalize();
+}
+
+
+
+
